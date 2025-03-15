c8e4b09122c174882f3239afbbe5c3ce11b56cdd
Sea25 <sonaabraham016@gmail.com>
2025-03-15 12:14:33 +0530

    Initial project setup

---
+++ herq-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to HerQ 💜</h1>
+      <p>Because She Deserves to Shine ✨</p>
+    </div>
+  );
+}
+
+export default App;
